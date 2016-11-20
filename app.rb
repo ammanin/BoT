0d778032778a640c972c59acb3837be86baa6f2d
ammanin <ammanin@andrew.cmu.edu>
2016-11-19 20:06:14 -0500

    pleaseee

--- app.rb
+++ app.rb
@@ -61,7 +61,7 @@ get '/incoming_sms' do
   body = params[:Body] || ""
   body = body.downcase.strip
   
- if body == "hi" or body == "hello" or body == "hey" or session["last_context"] == "start"
+ if body == "hi" or body == "hello" or body == "hey"
     message = get_about_message
  elsif body == "yes"
     session["last_context"] = "play"
@@ -77,6 +77,7 @@ get '/incoming_sms' do
 		session["answer_1"] = ""
 		session["answer_2"] = ""
 		session["last_context"] = "start"
+		end
 	end
 else
 
@@ -96,55 +97,7 @@ end
 	
 end
 
-=begin
-get '/incoming_sms' do
-  
-  session["last_context"] ||= nil
-  
-  sender = params[:From] || ""
-  body = params[:Body] || ""
-  body = body.downcase.strip
-  
-  if body == "hi" or body == "hello" or body == "hey"
-    message = get_about_message
-  elsif body == "play"
-    session["last_context"] = "play"
-    session["guess_it"] = rand(1...5)
-    message = "Guess what number I'm thinking of. It's between 1 and 5"
-  elsif session["last_context"] == "play"
-    
-    # if it's not a number 
-    if not body.to_i.to_s == body
-      message = "Cheater cheater that's not a number. Try again"
-    elsif body.to_i == session["guess_it"]
-      message = "Bingo! It was #{session["guess_it"]}"
-      session["last_context"] = "correct_answer"
-      session["guess_it"] = -1
-    else
-      message = "Wrong! Try again"
-    end
-    
-  elsif body == "who"
-    message = "I was made by Daragh."
-  elsif body == "what"
-    message = "I don't do much but I do it well. You can ask me who what when where or why."
-  elsif body == "when"    
-    message = Time.now.strftime( "It's %A %B %e, %Y")
-  elsif body == "where"    
-    message = "I'm in Pittsburgh right now."
-  elsif body == "why"    
-    message = "For educational purposes."
-  else 
-    message = error_response
-    session["last_context"] = "error"
-  end
-  
-  twiml = Twilio::TwiML::Response.new do |r|
-    r.Message message
-  end
-  twiml.text
-end
-=end
+
 get "/reset" do	
 	session["answer_1"]=""
 	session["answer_2"]=""
@@ -167,4 +120,3 @@ end
 def get_about_message
   get_greeting + ", I\'m nok-nok bot. Would you like to read a knock knock joke? Type Yes to play"
 end
-# ----------------------------------------------------------------------
